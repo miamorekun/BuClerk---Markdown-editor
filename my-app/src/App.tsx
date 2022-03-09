533bd4a4df23faa0fbbe2e569f18f9a89d6cd772
goethefau <buninmisha1809@gmail.com>
2022-03-09 12:14:51 +0200

    Add: "MUI Custom Theme & Custom palette"

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useTheme} from "@mui/styles";
+
+function App() {
+
+    return (
+        <div className="app">
+
+        </div>
+    );
+}
+
+export default App
