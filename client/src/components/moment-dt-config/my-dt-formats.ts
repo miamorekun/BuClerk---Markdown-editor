316c136b7e64536e3e067eb2ade1f53978b433ac
goethefau <buninmisha1809@gmail.com>
2022-03-31 14:17:00 +0200

    Reset

--- client/src/components/moment-dt-config/my-dt-formats.ts
+++
@@ -1,2 +0,0 @@
-export const date_time_format = "DD.MM.yyyy - hh:mm a"
-export const date_format = "DD.MM.yyyy"
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,39 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ThemeProvider} from '@mui/styles';
 import {themeLight} from './customMuiThemes';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DateAdapter from '@mui/lab/AdapterMoment';
 import "./index.scss"
-import {configure, spy} from "mobx";
-import MyNoteDescriptionModal from "./components/note-description-editor/MyNoteDescriptionModal";
-
-spy((ev) => {
-    if (ev.type === "action") {
-        console.log(ev)
-    }
-})
-
-setTimeout(() => {
-    configure({
-        reactionScheduler: (f) => {
-            setTimeout(f, 1);
-        },
-    });
-}, 1);
 
 ReactDOM.render(
     <React.StrictMode>
-        <LocalizationProvider dateAdapter={DateAdapter}>
-            <ThemeProvider theme={themeLight}>
-                <MyNoteDescriptionModal/>
-                <App/>
-            </ThemeProvider>
-        </LocalizationProvider>
+        <ThemeProvider theme={themeLight}>
+            <App/>
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
