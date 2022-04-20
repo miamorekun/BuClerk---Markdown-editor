f669a67ba363b7333c2d76ed8a7a1c4fe3a789e0
goethefau <buninmisha1809@gmail.com>
2022-04-20 16:58:30 +0200

    Update: "MyButton Component"

--- client/src/components/button/MyButton.tsx
+++ client/src/components/button/MyButton.tsx
@@ -8,7 +8,7 @@ export const MyButton = withStyles((theme: ThemeOptions) => ({
         backgroundColor: `${theme.my.accent_light} !important`,
         color: `${theme.my.accent} !important`,
         borderRadius: "4px !important",
-        fontSize:14,
+        fontSize: 14,
         fontFamily: "Rubik-Medium",
     }
 }))(ButtonBase)
@@ -20,7 +20,7 @@ export const MyButtonError = withStyles((theme: ThemeOptions) => ({
         backgroundColor: `${theme.my.tomato_light} !important`,
         color: `${theme.my.tomato} !important`,
         borderRadius: "4px !important",
-        fontSize:14,
+        fontSize: 14,
         fontFamily: "Rubik-Medium",
     }
 }))(ButtonBase)
@@ -31,8 +31,41 @@ export const MyButtonSuccess = withStyles((theme: ThemeOptions) => ({
         height: 36,
         backgroundColor: `${theme.my.success_light} !important`,
         color: `${theme.my.success} !important`,
-        borderRadius: "4px !important",
-        fontSize:14,
+        borderRadius: 4,
+        fontSize: 14,
         fontFamily: "Rubik-Medium",
+    }
+}))(ButtonBase)
+
+export const MyButtonFilled = withStyles((theme: ThemeOptions) => ({
+    root: {
+        padding: "0 14px !important",
+        minWidth: 36,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 36,
+        backgroundColor: `${theme.my.text_100} !important`,
+        color: `${theme.my.text_500} !important`,
+        borderRadius: 4,
+        fontSize: 14,
+        fontFamily: "Rubik-Medium",
+    }
+}))(ButtonBase)
+
+export const MyButtonClear = withStyles((theme: ThemeOptions) => ({
+    root: {
+        minWidth: 36,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 36,
+        color: `${theme.my.text_500} !important`,
+        borderRadius: "50% !important",
+        fontSize: 14,
+        fontFamily: "Rubik-Medium",
+        "&:hover": {
+            backgroundColor: theme.my.text_100
+        }
     }
 }))(ButtonBase)
