56fc40414ab624462046e99a879fe945d9f66e2c
goethefau <buninmisha1809@gmail.com>
2022-04-20 16:57:22 +0200

    Update: "Main Editor Component"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import BCEditor from "./components/editor/Index";
 
 function App() {
     return (
-        <div className="bc-app">
-
+        <div className="d-flex justify-content-center w-100">
+            <div className="bc-markdown-editor p-4 w-50">
+                <BCEditor/>
+            </div>
         </div>
     );
 }
